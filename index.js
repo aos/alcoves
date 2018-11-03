e5f45707f6459ba9eee104fecfe4ffefdc5e508e
Aos <aosdab@gmail.com>
2018-11-03 17:58:30 -0400

    Added try/catch block

--- index.js
+++ index.js
@@ -6,31 +6,37 @@ const download = require('image-downloader');
 const IMAGE_DIRECTORY = '../../../Pictures/covers';
 
 const scrapeImgUrls = async () => {
-  console.log('🚀  Launching...');
-  // Launches puppeteer and goes to URL
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  try {
+    console.log('🚀  Launching...');
+    // Launches puppeteer and goes to URL
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  console.log('🌎  Visiting web page...');
-  await page.goto('https://old.reddit.com/r/fakealbumcovers/');
+    console.log('🌎  Visiting web page...');
+    await page.goto('https://old.reddit.com/r/fakealbumcovers/');
 
-  console.log('⛏  Scraping image URLs...');
-  const results = await page.evaluate(() => {
-    // Grabs all <divs> of images
-    const elements = document
-          .querySelectorAll('.content > .spacer > #siteTable > .thing');
-    /* 
-     * Spreads elements into an array to filter, then creates a new array of
-     * only the URLs
-     * You can customize this to add another filter below the first if 
-     * looking for more specific criteria -- refer to blog post
-    */
-    return [...elements]
-           .filter(el => el.dataset.url.search(/(.jpg)|(.png)/) >= 0)
-           .map(el => el.dataset.url)
-  });
-  browser.close();
-  return results;
+    console.log('⛏  Scraping image URLs...');
+    const results = await page.evaluate(() => {
+      // Grabs all <divs> of images
+      const elements = document
+        .querySelectorAll('.content > .spacer > #siteTable > .thing');
+      /* 
+       * Spreads elements into an array to filter, then creates a new array of
+       * only the URLs
+       * You can customize this to add another filter below the first if 
+       * looking for more specific criteria -- refer to blog post
+       */
+      return [...elements]
+        .filter(el => el.dataset.url.search(/(.jpg)|(.png)/) >= 0)
+        .map(el => el.dataset.url)
+    });
+    browser.close();
+    return results;
+  }
+  catch(e) {
+    console.log(e);
+    throw e;
+  }
 }
 
 const downloadImg = async (options = {}) => {
@@ -46,13 +52,20 @@ const downloadImg = async (options = {}) => {
 const downloadAll = async () => {
   const imgs = await scrapeImgUrls();
 
-  await Promise.all(imgs.map(async (file) => {
-    await downloadImg({
-      url: file,
-      dest: IMAGE_DIRECTORY 
-    });
-  }));
-  console.log(`👌  Done -- downloaded \x1b[36m${imgs.length}\x1b[0m album covers!`);
+  try {
+    await Promise.all(imgs.map(async (file) => {
+      await downloadImg({
+        url: file,
+        dest: IMAGE_DIRECTORY 
+      });
+    }));
+
+    console.log(`👌  Done -- downloaded \x1b[36m${imgs.length}\x1b[0m album covers!`);
+  }
+  catch(e) {
+    console.log(e);
+    throw e;
+  }
 }
 
 downloadAll();
